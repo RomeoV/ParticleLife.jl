8b7b5ceea822141c408827e6038cb4d5e42cb64c
Romeo Valentin <romeov@stanford.edu>
2023-12-31 05:34:45 -0800

    Add fourth color and change layout.

--- src/ParticleLife.jl
+++ src/ParticleLife.jl
@@ -7,8 +7,9 @@ using FLoops
 using FoldsThreads
 using DataStructures: OrderedDict
 import LinearAlgebra: norm
+using Makie
 
-export agent_step!, make_model, color_sym
+export agent_step!, make_model, color_sym, run_sim
 
 greet() = print("Hello World!")
 
@@ -24,13 +25,12 @@ struct Orange <: ParticleColor end
 end
 
 function make_model()
-    extent::NTuple{2, Float64} = 2 .* (500.0, 500.0);
+    extent::NTuple{2, Float64} = 4 .* (500.0, 500.0);
     space2d = ContinuousSpace(extent;
                               periodic=false,
                               # update_vel! = update_vel!,
                               spacing=5,
                               );
-                              # spacing=minimum(extent)/200)
     model = AgentBasedModel(Particle, space2d;
                             agent_step! = agent_step!, model_step! = model_step!,
                             properties=Dict{Symbol, Float64}(
@@ -41,7 +41,7 @@ function make_model()
                                             :green_red     =>0.55,
                                             :green_green   =>-0.32,
                                             :green_yellow  =>-0.41,
-                                            :cyan_cyan     =>0.76,
+                                            :green_cyan     =>0.76,
                                             :yellow_red    =>-0.93,
                                             :yellow_green  =>0.86,
                                             :yellow_yellow =>-0.56,
@@ -55,7 +55,7 @@ function make_model()
                             ))
 
     for c in [Red(), Green(), Yellow(), Cyan()]
-        for _ in 1:200
+        for _ in 1:1000
             # vel =  SVector(rand() * 400 + 50, rand() * 400 + 50)
             vel =  SVector(0., 0.)
             add_agent!(model, vel, c)
@@ -66,21 +66,6 @@ function make_model()
 end
 
 color_interact(lhs::ParticleColor,  rhs::ParticleColor, m::ABM) = abmproperties(m)[Symbol(string(color_sym(lhs))*'_'*string(color_sym(rhs)))]
-    # color_interact(lhs, rhs)
-# color_interact(::Red,  ::Red, m::ABM) = abmproperties(m)[:red_red]
-# color_interact(::ParticleColor,  ::ParticleColor) = 0;
-# # Green with
-# color_interact(::Green,  ::Green)  = -0.32
-# color_interact(::Green,  ::Red)    = 0.55;
-# color_interact(::Green,  ::Yellow) = -0.41;
-# # Red with
-# color_interact(::Red,    ::Green)  = 0.65;
-# color_interact(::Red,    ::Red)    = 0.64;
-# color_interact(::Red,    ::Yellow) = 0.76;
-# # Yellow with
-# color_interact(::Yellow, ::Red  )  = -0.93;
-# color_interact(::Yellow, ::Green)  = 0.86;
-# color_interact(::Yellow, ::Yellow) = -0.56;
 
 color_sym(p::Particle) = color_sym(p.color)
 color_sym(::ParticleColor) = :black
@@ -91,10 +76,13 @@ color_sym(::Cyan) = :cyan
 
 function model_step!(model)
     total_vel = 0.
-    @floop for agent in allagents(model)
-    # for agent in allagents(model)
-        update_vel!(agent, model)
-        @reduce(total_vel += norm(agent.vel))
+    @floop begin
+        for agent in allagents(model)
+        # for agent in allagents(model)
+            update_vel!(agent, model)
+            # @reduce(total_vel += norm(agent.vel))
+            total_vel += norm(agent.vel)
+        end
     end
     total_vel /= length(allagents(model))
     if total_vel > 30.
@@ -120,29 +108,64 @@ function update_vel!(agent::Particle, model::ABM)
     # push away from border
     force += 0.1*(max.(40 .- agent.pos, 0)
                  - max.(40 .- (spacesize(model) - agent.pos), 0))
-    # vel = let viscosity = 0.1
-    #     vel_ = convert(MVector, middle.(agent.vel, force))
-    #     vel_ .*= (1-viscosity)
-    #     clamp!(vel_, -400, 400)
-    #     return SVector(vel_...)
-    # end
-    # @show vel
+
+    # combine past velocity and current force
     viscosity = abmproperties(model)[:viscosity]
-    # agent.vel = middle.(agent.vel, force) * (1-viscosity)
     agent.vel = agent.vel * (1-viscosity) + force
+end
+
+function run_sim()
+    fig, ax, abmobs = abmplot(make_model();
+                              ac=color_sym, as=4.0,  # agent color and size
+                              params=ParticleLife.properties)
+    # fig[1,2] = content(fig[2,1])
+    controls = content(fig[2,1][1,1])
+    param_sliders = content(fig[2,1][1,2])
+    update_button = content(param_sliders[2,1])
+
+    # fig[1,1] = ax
+    ax.width[] = 1000
+    ax.height[] = 1500
+    ui = fig[1,2] = GridLayout();
+    param_sliders.width[] = 500
+    ui[1,1] = param_sliders
+    sg::SliderGrid = content(param_sliders[1,1])
+    update_and_rand_button = param_sliders[2,1] = GridLayout();
+    update_and_rand_button[1,1] = update_button
+    rand_button = Button(update_and_rand_button[1,2], label="randomize", tellwidth=true)
+    on(rand_button.clicks) do _
+        for s in sg.sliders
+            set_close_to!(s, rand(s.range[]))
+        end
+        update_button.clicks[] += 1
+    end
+
+    Label(ui[2,1], "----------------------")
+    ui[3,1] = controls
+    Label(ui[4,1], "----------------------")
+
+    Makie.deleterow!(fig.layout, 2)
+    fig
 end
 
 properties=OrderedDict(
     :red_red       => -1:0.1:1,
     :red_green     => -1:0.1:1,
     :red_yellow    => -1:0.1:1,
+    :red_cyan      => -1:0.1:1,
     :green_red     => -1:0.1:1,
     :green_green   => -1:0.1:1,
     :green_yellow  => -1:0.1:1,
+    :green_cyan    => -1:0.1:1,
     :yellow_red    => -1:0.1:1,
     :yellow_green  => -1:0.1:1,
     :yellow_yellow => -1:0.1:1,
-    :viscosity     => 0.0:0.01:1.0,
+    :yellow_cyan   => -1:0.1:1,
+    :cyan_red      => -1:0.1:1,
+    :cyan_green    => -1:0.1:1,
+    :cyan_yellow   => -1:0.1:1,
+    :cyan_cyan     => -1:0.1:1,
+    :viscosity     =>  0:.01:1,
 )
 
 end # module ParticleLife

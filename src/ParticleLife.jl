2398228a867953a70182250ae11f700e7e89368f
Romeo Valentin <romeov@stanford.edu>
2023-12-31 07:01:00 -0800

    Track fps

--- src/ParticleLife.jl
+++ src/ParticleLife.jl
@@ -7,7 +7,8 @@ using FLoops
 using FoldsThreads
 using DataStructures: OrderedDict
 import LinearAlgebra: norm
-using Makie
+using Makie, Makie.Observables
+using OnlineStats
 
 export agent_step!, make_model, color_sym, run_sim
 
@@ -28,33 +29,18 @@ function make_model()
     extent::NTuple{2, Float64} = 4 .* (500.0, 500.0);
     space2d = ContinuousSpace(extent;
                               periodic=false,
-                              # update_vel! = update_vel!,
-                              spacing=5,
+                              spacing=20,
                               );
     model = AgentBasedModel(Particle, space2d;
-                            agent_step! = agent_step!, model_step! = model_step!,
-                            properties=Dict{Symbol, Float64}(
-                                            :red_red       =>0.64,
-                                            :red_green     =>0.65,
-                                            :red_yellow    =>0.76,
-                                            :red_cyan      =>0.76,
-                                            :green_red     =>0.55,
-                                            :green_green   =>-0.32,
-                                            :green_yellow  =>-0.41,
-                                            :green_cyan     =>0.76,
-                                            :yellow_red    =>-0.93,
-                                            :yellow_green  =>0.86,
-                                            :yellow_yellow =>-0.56,
-                                            :yellow_cyan   =>-0.56,
-                                            :cyan_red      =>-0.93,
-                                            :cyan_green    =>0.86,
-                                            :cyan_yellow   =>-0.56,
-                                            :cyan_cyan     =>0.76,
-                                            :time_scale    => 1.0,
-                                            :viscosity     => 0.1,
-                            ))
+                            agent_step! = agent_step!,
+                            model_step! = model_step!,
+                            properties=push!(Dict{Symbol, Float64}(
+                                                lhs_rhs=>rand(range)
+                                                for (lhs_rhs, range) in properties),
+                                            :time_scale => 1.0)
+                            )
 
-    for c in [Red(), Green(), Yellow(), Cyan()]
+    for c in [Red(), Green(), Orange(), Cyan()]
         for _ in 1:1000
             # vel =  SVector(rand() * 400 + 50, rand() * 400 + 50)
             vel =  SVector(0., 0.)
@@ -62,6 +48,9 @@ function make_model()
         end
     end
     @assert all(0 <= agent.pos[i] <= extent[i] for agent in allagents(model), i=1:2)
+
+    push!(last_model_step_time, model => time_ns())
+    push!(avg_model_step_duration, model=>Observable(Mean()))
     model
 end
 
@@ -71,18 +60,20 @@ color_sym(p::Particle) = color_sym(p.color)
 color_sym(::ParticleColor) = :black
 color_sym(::Green) = :green
 color_sym(::Red) = :red
-color_sym(::Yellow) = :yellow
+color_sym(::Orange) = :orange
 color_sym(::Cyan) = :cyan
+color_sym(::Yellow) = :yellow
+
+last_model_step_time = IdDict{StandardABM, UInt64}()
+avg_model_step_duration = IdDict{StandardABM, Observable{Mean}}()
 
 function model_step!(model)
     total_vel = 0.
-    @floop begin
-        for agent in allagents(model)
+    @floop for agent in collect(allagents(model))
         # for agent in allagents(model)
             update_vel!(agent, model)
-            # @reduce(total_vel += norm(agent.vel))
-            total_vel += norm(agent.vel)
-        end
+            @reduce(total_vel += norm(agent.vel))
+            # total_vel += norm(agent.vel)
     end
     total_vel /= length(allagents(model))
     if total_vel > 30.
@@ -94,12 +85,18 @@ function model_step!(model)
         model.time_scale /= 1.01
     end
     @debug model.time_scale
+
+    delta_time = time_ns() - last_model_step_time[model]
+    last_model_step_time[model] = time_ns()
+    fit!(avg_model_step_duration[model][], delta_time/1e9)
+    notify(avg_model_step_duration[model])
 end
 
 agent_step!(agent, model) = move_agent!(agent, model, abmproperties(model)[:time_scale])
 function update_vel!(agent::Particle, model::ABM)
     force = sum(
-        let g = color_interact(agent.color, other.color, model), d = euclidean_distance(agent, other, model)
+        let g = color_interact(agent.color, other.color, model),
+            d = euclidean_distance(agent, other, model)
             (0 < d < 80 ? (g / d .* (agent.pos - other.pos)) : SVector(0., 0.))
         end
         for other in Agents.nearby_agents(agent, model, 80);
@@ -115,9 +112,14 @@ function update_vel!(agent::Particle, model::ABM)
 end
 
 function run_sim()
-    fig, ax, abmobs = abmplot(make_model();
-                              ac=color_sym, as=4.0,  # agent color and size
-                              params=ParticleLife.properties)
+    # this is basically it, but we want to rearrange the layout a bit.
+    model = make_model()
+    fig, ax, abmobs = with_theme(theme_dark()) do
+        abmplot(model;
+                ac=color_sym, as=8.0,  # agent color and size
+                params=ParticleLife.properties,
+                scatterkwargs=(; :markerspace=>:data))
+    end
     # fig[1,2] = content(fig[2,1])
     controls = content(fig[2,1][1,1])
     param_sliders = content(fig[2,1][1,2])
@@ -143,6 +145,11 @@ function run_sim()
     Label(ui[2,1], "----------------------")
     ui[3,1] = controls
     Label(ui[4,1], "----------------------")
+    fps = throttle(0.5, @lift 1/value($(avg_model_step_duration[model])))
+    fps_label = Label(ui[5,1], text="0.0 fps")
+    on(fps) do fps
+        fps_label.text = "$(round(fps, digits=2)) fps"
+    end
 
     Makie.deleterow!(fig.layout, 2)
     fig
@@ -151,19 +158,19 @@ end
 properties=OrderedDict(
     :red_red       => -1:0.1:1,
     :red_green     => -1:0.1:1,
-    :red_yellow    => -1:0.1:1,
+    :red_orange    => -1:0.1:1,
     :red_cyan      => -1:0.1:1,
     :green_red     => -1:0.1:1,
     :green_green   => -1:0.1:1,
-    :green_yellow  => -1:0.1:1,
+    :green_orange  => -1:0.1:1,
     :green_cyan    => -1:0.1:1,
-    :yellow_red    => -1:0.1:1,
-    :yellow_green  => -1:0.1:1,
-    :yellow_yellow => -1:0.1:1,
-    :yellow_cyan   => -1:0.1:1,
+    :orange_red    => -1:0.1:1,
+    :orange_green  => -1:0.1:1,
+    :orange_orange => -1:0.1:1,
+    :orange_cyan   => -1:0.1:1,
     :cyan_red      => -1:0.1:1,
     :cyan_green    => -1:0.1:1,
-    :cyan_yellow   => -1:0.1:1,
+    :cyan_orange   => -1:0.1:1,
     :cyan_cyan     => -1:0.1:1,
     :viscosity     =>  0:.01:1,
 )
